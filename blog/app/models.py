489e38e1864f1dd5f68a16f65bdb53d6f59dd054
leahcimsu <leahcimsu@tencent.com>
2021-11-15 18:16:37 -0500

    chore: fields reminder

--- blog/app/models.py
+++ blog/app/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 class Blog2(models.Model):
   title =  models.CharField(unique=True, max_length=50, blank=False)
+  # subtitle
   body = models.TextField()
   user_id = models.IntegerField(default=000)
+  # user_name
   tag = models.CharField(max_length=50,default='NAN')
+  # Time format: Unix Timestamp
   create_time = models.DateTimeField(auto_now_add=True)
   update_time = models.DateTimeField(auto_now=True)
